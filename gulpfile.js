4118d62ad98a7f948bd6afeb109cdd04e37b80ff
rory chou <zhouhaotian@lvmama.com>
2017-05-19 16:56:47 +0800

    Signed-off-by: rory chou <zhouhaotian@lvmama.com>

---
+++ gulpfile.js
@@ -0,0 +1,212 @@
+/**
+ * Created by rory on 2017/3/8.
+ */
+const gulp = require('gulp'),
+      Path = require('path'),
+      sass = require('gulp-sass'),
+      gulpIf = require('gulp-if'),
+      uglify = require('gulp-uglify'),
+      clean = require('gulp-clean'),
+      replace = require('gulp-replace'),
+      htmlReplace = require('gulp-html-replace'),
+      cleanCss = require('gulp-clean-css'),
+      spriter = require('gulp-css-spriter'),
+      prefix = require("gulp-prefix"),
+      browserSync = require('browser-sync').create(),
+      reload = browserSync.reload,
+      autoprefixer = require("gulp-autoprefixer"),
+      rev = require('gulp-rev'),
+      collector = require('gulp-rev-collector');
+
+let htmlNameArr = ['lvcc','BrandCooperation'],
+    htmlName = 'home',//
+    htmlNameAll = '*',//
+    htmlSvn = 'test',
+    cssSvn = 'test',
+    jsSvn = 'test',
+    imgsSvn = 'test',
+    img = 'http://pic.lvmama.com/img/lvcc/',
+    css = 'http://pic.lvmama.com/styles/lvcc/',
+    js = 'http://pic.lvmama.com/js/lvcc/',
+    folderName = 'http://pic.lvmama.com/styles/zt/9years',
+    shouldSprite = false,
+    shouldMd5 = false,
+    shouldUglify = true,
+    shouldPrefix = false,
+    obj = {
+        domains:['s1.lvjs.com.cn','s2.lvjs.com.cn','s3.lvjs.com.cn','pic.lvmama.com'],
+        protocol:'http://'
+    };
+
+// 静态服务器 + 监听 scss/html/js/imgs 文件
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: {
+            baseDir: "./src",
+            index: htmlName+".html"
+        }
+    });
+    gulp.watch("src/scss/*.scss", ['sass']);
+    gulp.watch("src/*.html").on('change', reload);
+    gulp.watch("src/js/*.js").on('change', reload);
+    gulp.watch("src/imgs/*").on('change', reload);
+});
+
+// scss编译后的css将注入到浏览器里实现更新
+gulp.task('sass', function() {
+    return gulp.src("src/scss/*.scss")
+        .pipe(sass({outputStyle:'compact'}).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest("src/css"))
+        .pipe(reload({stream: true}));
+});
+
+//dist-js，压缩
+gulp.task('dist-js',function () {
+    gulp.src('dist/js/*.js', {read: false})
+        .pipe(clean());
+    return gulp.src('src/js/*.js')
+        .pipe(gulpIf(shouldUglify,uglify()))
+        .pipe(gulpIf(shouldMd5,rev()))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(gulpIf(shouldMd5,rev.manifest()))
+        .pipe(gulpIf(shouldMd5,gulp.dest('rev/js')));
+});
+
+//dist-imgs
+gulp.task('dist-img',['dist-css'],function () {
+    gulp.src('dist/imgs/*', {read: false})
+        .pipe(clean());
+    return gulp.src(['src/imgs/*','src/imgs/*/*','!src/imgs/psd','!src/imgs/psd/*','!src/imgs/sprite','!src/imgs/sprite/*'])
+        .pipe(gulp.dest('dist/imgs'));
+});
+
+//判断是否需要雪碧图
+gulp.task('checkSprite',function () {
+    gulp.src('src/imgs/sprite/*')
+        .on('data',function (file) {
+            file ? shouldSprite = true:shouldSprite = false;
+        })
+});
+
+//dist-css,生成雪碧图,压缩
+gulp.task('dist-css',['sass','checkSprite'],function () {
+    gulp.src('dist/css/*.css', {read: false})
+        .pipe(clean());
+    return gulp.src('src/css/*.css')
+        .pipe(gulpIf(shouldSprite,spriter({
+            'spriteSheet': './src/imgs/spritesheet.png',
+            'pathToSpriteSheetFromCSS': '../imgs/spritesheet.png',
+            'includeMode':'explicit'
+        })))
+        .pipe(cleanCss({
+            compatibility:'ie7',
+            format:{
+                breaks:{
+                    afterRuleEnds: true
+                }
+            }
+        }))
+        .pipe(gulpIf(shouldMd5,rev()))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(gulpIf(shouldMd5,rev.manifest()))
+        .pipe(gulpIf(shouldMd5,gulp.dest('rev/css')));
+});
+
+//dist-html
+gulp.task('dest-html',function () {
+    gulp.src('src/'+htmlName+'.html')
+        .pipe(gulpIf(shouldPrefix,prefix(folderName,null,'{{')))
+        //替换lazyload中的图片路径
+        .pipe(gulpIf(shouldPrefix,replace('data-original="imgs/','data-original="'+folderName+'/imgs/')))
+        .pipe(gulp.dest('dist'));
+});
+
+//dest-dist
+gulp.task('dist',['dist-js','dist-img','dist-css','dest-html'],function () {
+    if(shouldMd5){
+        return gulp.src(['rev/**/*.json','dist/'+htmlName+'.html'])
+            .pipe(collector({
+                replaceReved: true,
+                dirReplacements: {
+                    'css': 'css',
+                    'js': 'js'
+                }
+            }))
+            .pipe(gulp.dest('dist'))
+    }else {
+        return gulp.src('dist/*');
+    }
+});
+
+//css相对路径替换绝对路径
+gulp.task('cssReplace',['dist'],function () {
+    return gulp.src('dist/css/*.css')
+             .pipe(replace(/\.\.\/imgs/g,img))
+             .pipe(gulp.dest('dist/css'));
+});
+
+//html相对路径替换绝对路径
+gulp.task('urlReplace',['dist'],function () {
+    return gulp.src('dist/'+htmlName+'.html')
+            .pipe(replace(/imgs\//g,img))
+            .pipe(replace(/\=\"css\//g,'="'+css))
+            .pipe(replace(/\=\"js\//g,'="'+js))
+             .pipe(gulp.dest('dist'))
+});
+
+//获取资源路径
+gulp.task('getUrl',['urlReplace'],function () {
+    return gulp.src('dist/'+htmlName+'.html')
+        .on('data',function (file) {
+        let fileName = file.relative;
+            obj[fileName] = {};
+            obj[fileName].css = [];
+            obj[fileName].js = [];
+        let string = JSON.stringify(file.contents.toString('utf-8')),
+            regexpComment = /(\n?)([ \t]*)(<!--\s*build:(\w+(?:-\w+)*)\s*-->)\n?([\s\S]*?)\n?(<!--\s*endbuild\s*-->)\n?/ig,
+            regexpHref = /href\s*=\s*\\"(.*)\\"\s*\/*>/i,
+            regexpSrc = /src\s*=\s*\\"(.*)\\"\s*>/i,
+            regexpMin = /index\.php\?f=(.*)$/i,
+            regexpIndex = /pic\.lvmama\.com(.*)$/i,
+            commentArr = string.match(regexpComment);//获取所有此注释的片段数组
+
+        //遍历数组,判断build:的标识，并保存
+        for(let i in commentArr){
+            let line = commentArr[i].split('\\r\\n');
+            for(let j=1;j < line.length-1;j++){
+                if(line[0].match('css')){
+                    let res = line[j].match(regexpHref);
+                    res[1].match(regexpMin)?obj[fileName].css.push(res[1].match(regexpMin)[1]):obj[fileName].css.push(res[1].match(regexpIndex)[1]);
+                }else if(line[0].match('js')){
+                    let res = line[j].match(regexpSrc);
+                    res[1].match(regexpMin)?obj[fileName].js.push(res[1].match(regexpMin)[1]):obj[fileName].js.push(res[1].match(regexpIndex)[1]);
+                }else {
+                    console.log('error')
+                }
+            }
+        }
+    })
+});
+
+//html路径合并
+gulp.task('urlConcat',['getUrl'],function () {
+    return gulp.src('dist/'+htmlName+'.html')
+            .pipe(htmlReplace({
+                'css':'<link rel="stylesheet" href="'+ obj.protocol +obj.domains[Math.floor(Math.random()*4)] + '/min/index.php?f=' + obj[fileName].css.join(',') +'">',
+                'js':'<script src="'+ obj.protocol +obj.domains[Math.floor(Math.random()*4)] + '/min/index.php?f=' + obj[fileName].js.join(',') +'"></script>'
+            }))
+            .pipe(gulp.dest('dist'))
+});
+
+//svn
+gulp.task('dest-svn',['urlConcat','cssReplace'],function () {
+    gulp.src('dist/js/*.js')
+        .pipe(gulp.dest(jsSvn));
+    gulp.src('dist/css/*.css')
+        .pipe(gulp.dest(cssSvn));
+    gulp.src('dist/imgs/*')
+        .pipe(gulp.dest(imgsSvn));
+    gulp.src('dist/'+htmlName+'.html')
+        .pipe(gulp.dest(htmlSvn));
+});
---
+++ src/js/index.js
@@ -0,0 +1,223 @@
+/**
+ * Created by rory on 2017/5/18.
+ */
+$(function () {
+    var $document = $(document);
+
+    var search = {
+        suggestionFinished:false,
+        target:null,
+        container:$('.search-container'),
+        suggestionBox: $('.drop-suggestion-citys'),
+        completeBox: $('.drop-complete'),
+        init: function () {
+            this.bindEvent();
+            //tab切换
+            tabSwitch('.letter-tabs li','.letter-city-contents li');
+            this.calendatInit();
+            this.getDefaultInfo();
+        },
+        getDefaultInfo:function () {
+            //填写默认信息
+        },
+        getPosition: function () {
+            //获取container的位置
+            var dropT = this.target.offset().top-this.container.offset().top+this.target.height();
+            var dropL = this.target.offset().left-this.container.offset().left;
+            if(this.target.hasClass('search-city')){
+                this.suggestionBox.css({
+                    top: dropT,
+                    left: dropL
+                });
+                this.completeBox.css({
+                    top: dropT,
+                    left: dropL
+                });
+            }else {
+                //弹出calendar
+            }
+        },
+        calendatInit: function () {
+            var self = this;
+            lv.calendar({
+                date: "2017-05-19",
+                autoRender: false,
+                trigger: ".search-date",
+                triggerEvent: "click",
+                bimonthly: true,
+                //定位偏移
+                monthNext: 10,
+                monthPrev: 10,
+                dayPrev: 0,
+                template: "small",
+                //点击选择日期后的回调函数 默认返回值: calendar对象
+                selectDateCallback: function () {
+                    for(var i in this.selected){
+                        self.target.find('input').val(i)
+                        //判断星期几并写入info
+                        var date = new Date(i);
+                        var week = ['周日','周一','周二','周三','周四','周五','周六']
+                        var weekDay = week[date.getDay()];
+                        self.target.find('.search-contents-info').html(weekDay);
+                    }
+                }
+            })
+        },
+        bindEvent: function () {
+            var self = this;
+            //点击section
+            $document.on('click','.section-input',function (e) {
+                console.log('section-input:clicked')
+                e.stopPropagation();
+                self.target = $(e.target).hasClass('section-input')?$(e.target):$(e.target).parent('.section-input');
+                //判断是否需要drop
+                if(!self.target.hasClass('search-city') && !self.target.hasClass('search-date')){
+                    return
+                }
+                self.getPosition();
+                //判断是suggestion还是calendar
+                if(self.target.hasClass('search-city')){
+                    self.getData('data/citys.json',{},'click');
+                }else if(self.target.hasClass('search-date')){
+
+                }
+            });
+            //drop中选择城市
+            $document.on('click','.drop-city',function (e) {
+                var value = $(this).html();
+                //填写在input内,判断是suggestion还是complete
+                var input = self.target.hasClass('section-input')?self.target.find('input'):self.target;
+                input.val(value);
+                self.suggestionBox.hide();
+            });
+            //drop中切换城市列表
+            $document.on('click','.letter-tabs',function (e) {
+                e.stopPropagation();
+            });
+            var inputBox = self.target && self.target.find('input');
+            //input输入
+            $document.on('input',inputBox,function (e) {
+                console.log('input:input')
+                self.target = $(e.target);
+                var value = $(this).val();
+                self.getData('data/citys.json',{},'input');
+            });
+            //点击加数量
+            $document.on('input','.num-add',function (e) {
+                var inputBox = $(this).parent('.section-input').find('input');
+                var inputVal = inputBox.val()
+            });
+
+            //点击其他位置
+            $document.on('click',$document,function () {
+                self.suggestionBox.hide();
+                self.completeBox.hide();
+            });
+            //点击提交
+            $document.on('click','.search-btn',function () {
+                //表单验证
+
+                //setCookie
+
+                //跳转页面
+
+            })
+        },
+        getData: function (url,data,type) {
+            var self = this;
+            if(type === 'click'){
+                //判断是否已经加载过
+                if(!this.suggestionFinished){
+                    $.ajax({
+                        url:url,
+                        data: data,
+                        success: function (res) {
+                            self.renderData(res)
+                        },
+                        error: function (error) {
+                            console.log('error',error)
+                        }
+                    });
+                }else {
+                    self.suggestionBox.show();
+                }
+            }else {
+                //input 搜索
+                $.ajax({
+                    url:url,
+                    data: data,
+                    success: function (res) {
+                        self.renderData(res)
+                    },
+                    error: function (error) {
+                        console.log('error',error)
+                    }
+                });
+            }
+        },
+        renderData: function (res) {
+            //根据target判断如何渲染数据
+            if(this.target.hasClass('search-city')){
+                var hotCitys = res.hot;
+                var allCitys = res.all;
+                for(var i in hotCitys){
+                    var li = $('<li class="drop-city"></li>');
+                    li.html(hotCitys[i]);
+                    this.suggestionBox.find('.city-hot').append(li)
+                }
+                var dts = $('.letter-city-contents').find('dt');
+                dts.each(function () {
+                    var letter = $(this).html().toLowerCase();
+                    var dl = $(this).parent('dl');
+                    for(var j in allCitys[letter]){
+                        var dd = $('<dd class="drop-city"></dd>');
+                        dd.html(allCitys[letter][j]);
+                        dl.append(dd);
+                    }
+                });
+                this.suggestionFinished = true;
+                this.suggestionBox.show();
+            }else if(this.target.hasClass('search-date')){
+                //弹出日历
+
+            }else {
+                //input事件
+                //清除当前内容
+                this.completeBox.empty();
+                var resCitys = res.hot;
+                for(var i in resCitys){
+                    var li = $('<li class="drop-city"></li>');
+                    li.html(resCitys[i]);
+                    this.completeBox.append(li)
+                }
+                this.completeBox.show();
+            }
+        },
+        setCookie: function (info) {
+
+        }
+    };
+    //search.init();
+
+    /**
+     * @param
+     * tabs tabs对应的jq选择器
+     * details 相应tabs点击的内容容器jq选择器
+     */
+    function tabSwitch(tabs, details,shouldInitSearch) {
+        shouldInitSearch = shouldInitSearch||false;
+        //添加默认样式
+        $(tabs).eq(0).addClass("current").siblings(tabs).removeClass("current");
+        $(details).eq(0).show().siblings(details).hide();
+        shouldInitSearch && search.init();
+        //点击切换
+        $(tabs).click(function () {
+            $(this).addClass("current").siblings(tabs).removeClass("current");
+            console.log($(details).eq($(this).index()));
+            $(details).eq($(this).index()).show().siblings(details).hide();
+            shouldInitSearch && search.init();
+        })
+    }
+
+    tabSwitch('.search-tabs>li','.search-contents>li',true)
+});
