189f675a436a950de4d4b645c1a29da7c158eab0
rory chou <zhouhaotian@lvmama.com>
2017-05-23 17:53:48 +0800

    Signed-off-by: rory chou <zhouhaotian@lvmama.com>

--- src/js/index.js
+++ src/js/index.js
@@ -6,33 +6,45 @@ $(function () {
 
     var search = {
         target:null,
+        targetInput:null,
         container:$('.search-container'),
-        contentsLi:null,
-        suggestionBox: null,
+        suggestionBox: $('.drop-suggestion-citys'),
+        selectBox: $('.search-contents-selections'),
         calendar:null,
+        calendarFlightReturn:null,
         tabSwitcher:null,
-        startDate:null,
+        localCity:'上海',
         completeBox: $('.drop-complete'),//当前页面是否唯一？
         init: function () {
-            this.suggestionBox = $('.drop-suggestion-citys');
+            //this.suggestionBox = $('.drop-suggestion-citys');
             this.bindEvent();
-            this.tabSwitcher = new tabSwitch('.letter-tabs li','.letter-city-contents li');
-            this.calendatInit();
+            this.tabSwitcherCombo = this.tabSwitch('.search-contents-combo .letter-tabs li','.search-contents-combo .letter-city-contents li');
+            this.tabSwitcherFlight = this.tabSwitch('.search-contents-flight .letter-tabs li','.search-contents-flight .letter-city-contents li');
             this.getDefaultInfo();
+            this.calendarInit();
         },
         getDefaultInfo:function () {
             //填写默认信息
-            this.startDate = $('.combo-date').find('input').val()||this.dateNow();
+            $('.combo-date input').val(this.dateNow());
+            $('.flight-date-start input').val(this.dateNow());
+            $('.flight-date-return input').val(this.dateNow(Date.now()+86400000));
+            //默认出发地为本站站点
+            $('.combo-from input').val(this.localCity);
+            $('.flight-from input').val(this.localCity);
         },
         dateNow: function (set) {
             var date = set?new Date(set):new Date();
-            return date.toLocaleDateString();
+            var dateArr = date.toLocaleDateString().split('/');
+            //+0
+            dateArr[1] = dateArr[1]<10?'0'+dateArr[1]:dateArr[1];
+            dateArr[2] = dateArr[2]<10?'0'+dateArr[2]:dateArr[2];
+            return dateArr.join('-');
         },
         getPosition: function () {
             //获取container的位置
-            var dropT = this.target.offset().top-this.container.offset().top+this.target.height();
-            var dropL = this.target.offset().left-this.container.offset().left;
-            if(this.target.hasClass('search-city')){
+            var dropT = this.targetInput.offset().top-this.container.offset().top+this.targetInput.height();
+            var dropL = this.targetInput.offset().left-this.container.offset().left;
+            if(this.targetInput.hasClass('search-city')){
                 this.suggestionBox.css({
                     top: dropT,
                     left: dropL
@@ -45,12 +57,12 @@ $(function () {
                 //弹出calendar
             }
         },
-        calendatInit: function () {
+        calendarInit: function () {
             var self = this;
             this.calendar = lv.calendar({
-                date: "2017-05-19",
+                date: self.dateNow(),
                 autoRender: false,
-                trigger: ".search-date",
+                trigger: ".search-calendar-common",
                 triggerEvent: "click",
                 bimonthly: true,
                 //定位偏移
@@ -61,14 +73,37 @@ $(function () {
                 //点击选择日期后的回调函数 默认返回值: calendar对象
                 selectDateCallback: function () {
                     for(var i in this.selected){
-                        self.target.find('input').val(i)
+                        self.targetInput.find('input').val(i)
                         //判断星期几并写入info
                         var date = new Date(i);
                         var week = ['周日','周一','周二','周三','周四','周五','周六']
                         var weekDay = week[date.getDay()];
-                        self.target.find('.search-contents-info').html(weekDay);
-                        self.startDate = $('.combo-date').find('input').val();
+                        self.targetInput.find('.search-contents-info').html(weekDay);
+                        //刷新返程时间info
+                        self.freshInfo();
+                        //刷新返程的日历起始时间
+                        $('.flight-date-return input').val(self.dateNow((new Date(i)).getTime()+86400000))
                     }
+                }
+            });
+            this.calendarFlightReturn = lv.calendar({
+                autoRender: false,
+                trigger: ".search-cascading input",
+                triggerEvent: "click",
+                bimonthly: false,
+                //定位偏移
+                monthNext: 10,
+                monthPrev: 10,
+                dayPrev: 0,
+                template: "small",
+                cascading: true,
+                cascadingNextAuto: false,
+                //点击选择日期后的回调函数 默认返回值: calendar对象
+                selectDateCallback: function () {
+                    var week = ['周日','周一','周二','周三','周四','周五','周六']
+                    var date = new Date(self.targetInput.find('input').val());
+                    var weekDay = week[date.getDay()];
+                    self.targetInput.find('.search-contents-info').html(weekDay);
                 }
             })
         },
@@ -76,57 +111,59 @@ $(function () {
             var self = this;
             //点击search-city
             $document.on('click','.search-city',function (e) {
-                console.log('.search-city clicked',$(e.target));
                 var data = {};
-                e.stopPropagation();
-                self.target = $(e.target).hasClass('search-city')?$(e.target):$(e.target).parent('.search-city');
+                //e.stopPropagation();
+                self.targetInput = $(e.target).parent('.search-city');
                 self.getPosition();
                 //判断是何种请求
-                if(self.target.hasClass('combo-from')) {
+                if(self.targetInput.hasClass('combo-from')) {
                     data = {};
                     self.getData('data/citys.json',data,'click');
                 }
-                if(self.target.hasClass('flight-from')) {
+                if(self.targetInput.hasClass('flight-from')) {
                     data = {};
                     self.getData('data/citys.json',data,'click');
                 }
-                if(self.target.hasClass('combo-to')) {
+                if(self.targetInput.hasClass('combo-to')) {
                     data = {};
                     self.getData('data/citys.json',data,'click');
                 }
-                if(self.target.hasClass('flight-to')) {
+                if(self.targetInput.hasClass('flight-to')) {
                     data = {};
                     self.getData('data/citys.json',data,'click');
                 }
-
-                self.calendar && self.calendar.destroy();
+                //self.calendar && self.calendar.destroy();
             });
             //点击search-date
             $document.on('click','.search-date',function (e) {
-                e.stopPropagation();
-                self.target = $(e.target).hasClass('search-date')?$(e.target):$(e.target).parent('.search-date');
-                self.getPosition();
-                self.suggestionBox.hide();
-                self.completeBox.hide();
+                self.targetInput = $(e.target).parent('.search-date');
+                self.target = $(e.target);
             });
             //drop中选择城市
-            $document.on('click','.drop-city',function (e) {
-                e.stopPropagation();
+            $document.on('mousedown','.drop-city',function (e) {
+                console.log('.drop-city mousedown')
+                //e.stopPropagation();
                 var value = $(this).html();
-                //填写在input内,判断是suggestion还是complete
-                var input = self.target.hasClass('section-input')?self.target.find('input'):self.target;
+
+
+                //填写在input内,判断是suggestion还是complete???
+                var input = self.targetInput.find('input');
                 input.val(value);
+
+                //判断是在哪个tab下，初始化字母分组
+                if(self.targetInput.parent().hasClass('search-contents-combo')){
+                    self.tabSwitchInit('.search-contents-combo .letter-tabs li','.search-contents-combo .letter-city-contents li');
+                }else if(self.targetInput.parent().hasClass('search-contents-flight')){
+                    self.tabSwitchInit('.search-contents-flight .letter-tabs li','.search-contents-flight .letter-city-contents li');
+                }
+
                 self.suggestionBox.hide();
-                self.tabSwitcher.init();
                 self.completeBox.hide();
-            });
-            //drop中切换城市列表
-            $document.on('click','.letter-tabs',function (e) {
-                e.stopPropagation();
+                self.target = $(e.target);
             });
             //input输入
             $document.on('input','.section-input input',function (e) {
-                self.target = $(e.target).parent('.section-input');
+                self.targetInput = $(e.target).parent('.section-input');
                 if($(this).parent('.section-input').hasClass('search-date')||$(this).parent('.section-input').hasClass('search-city')){
                     self.getPosition();
                     //判断是否为空
@@ -139,28 +176,62 @@ $(function () {
             });
             //input on blur
             $document.on('blur','.section-input input',function (e) {
-                self.target = $(e.target).parent('.section-input');
-                if(!$(this).parent('.section-input').hasClass('search-date')&&!$(this).parent('.section-input').hasClass('search-city')){
+                console.log('blur');
+                //过滤suggestion内字母切换
+                if(!self.target.parent().hasClass('letter-tabs')){
+                    self.suggestionBox.hide();
+                    if(self.target.parent().hasClass('search-contents-combo')){
+                        self.tabSwitchInit('.search-contents-combo .letter-tabs li','.search-contents-combo .letter-city-contents li');
+                    }else if(self.target.parent().hasClass('search-contents-flight')){
+                        self.tabSwitchInit('.search-contents-flight .letter-tabs li','.search-contents-flight .letter-city-contents li');
+                    }
+                }
+                //self.target = $(e.target).parent('.section-input');
+                //过滤输入框，待优化
+                if(!$(this).parent('.section-input').hasClass('search-date')&&!$(this).parent('.section-input').hasClass('search-city')&&!$(this).hasClass('search-contents-select')){
                     var value = parseInt($(this).val())||0;
                     value = value>=0?value:-value;
                     self.numCalc(0,value)
                 }
+                //下拉菜单旋转箭头
+                if($(this).hasClass('search-contents-select')){
+                    $(this).siblings('b').removeClass('active');
+                }
+                //过滤suggestion中的点击
+
+                self.completeBox.hide();
+                self.selectBox.hide();
             });
             //点击加数量
             $document.on('click','.num-add',function () {
-                self.target = $(this).parent('.section-input');
+                self.targetInput = $(this).parent('.section-input');
                 self.numCalc(1)
             });
             //点击减数量
             $document.on('click','.num-minus',function () {
-                self.target = $(this).parent('.section-input');
+                self.targetInput = $(this).parent('.section-input');
                 self.numCalc(-1)
             });
-            //点击其他位置
-            $document.on('click',$document,function () {
-                console.log('hide')
-                self.suggestionBox.hide();
-                self.completeBox.hide();
+            //机票单程
+            $document.on('click','.flight-single',function () {
+                $(this).addClass('current').next().removeClass('current');
+                $('.flight-date-return').removeClass('search-cascading').addClass('disabled');
+            });
+            //下拉框点击
+            $document.on('click','.search-contents-select',function (e) {
+                self.target = $(e.target);
+                self.showSelectbox();
+            });
+            //下拉框内容确定
+            $document.on('mousedown','.search-contents-selections li',function () {
+                console.log('selections mousedown');
+                $(this).parent().siblings('b').removeClass('active');
+                $(this).parent().parent('.section-input').find('.search-contents-select').val($(this).html());
+            });
+            //机票返程
+            $document.on('click','.flight-double',function () {
+                $(this).addClass('current').siblings('.flight-single').removeClass('current');
+                $('.flight-date-return').addClass('search-cascading').removeClass('disabled');
             });
             //点击提交
             $document.on('click','.search-btn',function () {
@@ -173,11 +244,11 @@ $(function () {
             })
         },
         numCalc: function (move,inputValue) {
-            var inputBox = this.target.find('input');
+            var inputBox = this.targetInput.find('input');
             var max = inputBox.data('max');
             var min = inputBox.data('min');
-            var btnAdd = this.target.find('.num-add');
-            var btnMinus = this.target.find('.num-minus');
+            var btnAdd = this.targetInput.find('.num-add');
+            var btnMinus = this.targetInput.find('.num-minus');
             var oldValue = parseInt(inputBox.val());
             var newValue = inputValue === undefined?oldValue+move:inputValue;
 
@@ -198,13 +269,26 @@ $(function () {
             }
 
             //判断不同的规则
-            if(this.target.hasClass('combo-days')){
-                inputBox.val(newValue+'天')
-                this.target.find('.search-contents-info').html(this.startDate.substring(5)+'返回');
+            if(this.targetInput.hasClass('combo-days')){
+                inputBox.val(newValue+'天');
+                this.freshInfo()
             }else {
                 inputBox.val(newValue)
             }
 
+        },
+        showSelectbox: function () {
+            this.target.siblings('b').addClass('active');
+            this.target.siblings('.search-contents-selections').show();
+        },
+        freshInfo: function () {
+            var startDateStr = $('.combo-date').find('input').val();
+            var startDate = new Date(startDateStr);
+            var startTime = startDate.getTime();
+            var period = parseInt($('.combo-days input').val())*86400000;
+            var returnTime = new Date(startTime+period);
+            var returnDate = returnTime.toLocaleDateString().substring(5).replace(/\//,'-');
+            $('.combo-days .search-contents-info').html(returnDate+'返回')
         },
         getData: function (url,data,type) {
             var self = this;
@@ -274,6 +358,21 @@ $(function () {
                 console.log('search renderData error!')
             }
         },
+        tabSwitch: function (tabs, details) {
+            var self = this;
+            //添加默认样式
+            this.tabSwitchInit(tabs, details);
+            //点击切换
+            $(tabs).mousedown(function (e) {
+                self.target = $(e.target);
+                $(this).addClass("current").siblings(tabs).removeClass("current");
+                $(details).eq($(this).index()).show().siblings(details).hide();
+            })
+        },
+        tabSwitchInit: function (tabs, details) {
+            $(tabs).eq(0).addClass("current").siblings(tabs).removeClass("current");
+            $(details).eq(0).show().siblings(details).hide();
+        },
         setCookie: function (info) {
 
         }
@@ -294,7 +393,7 @@ $(function () {
         };
         this.init();
         //点击切换
-        $(tabs).click(function () {
+        $(tabs).mousedown(function () {
             $(this).addClass("current").siblings(tabs).removeClass("current");
             $(details).eq($(this).index()).show().siblings(details).hide();
         })
